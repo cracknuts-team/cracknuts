62af1bd028f5a97d988775f901bb4730820fb397
Dingzb <dingzibiao@gmail.com>
2024-08-07 11:11:11 +0800

    更新演示demo

--- tests/acquisition/acquisition_demo2.py
+++ tests/acquisition/acquisition_demo2.py
@@ -9,7 +9,8 @@ from cracknuts.cracker.basic_cracker import BasicCracker
 cracker = BasicCracker()
 logger.set_level(logging.WARNING, cracker)
 
-cracker.set_addr('192.168.0.12', 8080)
+# 11 为效果好的板子
+cracker.set_addr('192.168.0.11', 8080)
 cracker.connect()
 
 
@@ -19,9 +20,9 @@ def init(c: AbsCracker):
     c.cracker_nut_enable(1)
     time.sleep(2)
 
-    # set_key = '01 00 00 00 00 00 00 10 00 11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff'
+    set_key = '01 00 00 00 00 00 00 10 00 11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff'
     l = 6
-    set_key = '02 00 00 00 00 00 00 08 88 99 AA BB CC DD EE FF'
+    # set_key = '02 00 00 00 00 00 00 08 88 99 AA BB CC DD EE FF'
     set_key = set_key.replace(' ', '')
     set_key = bytes.fromhex(set_key)
     c.cracker_serial_data(l, set_key)
@@ -29,12 +30,14 @@ def init(c: AbsCracker):
 
 def do(c: AbsCracker):
     # enc
-    # d = '01 02 00 00 00 00 00 10 00 11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff'
+    d = '01 02 00 00 00 00 00 10 00 11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff'
     # l = '00 00 00 00 00 10 62 F6 79 BE 2B F0 D9 31 64 1E 03 9C A3 40 1B B2'
     # l = len(l.split(' '))
-    d = '02 02 00 00 00 00 00 08 88 99 AA BB CC DD EE FF'
-    l = '00 00 00 00 00 08 97 9F FF 9B 97 0C A6 A4'
-    l = len(l.split(' '))
+    l = 22
+    # d = '02 02 00 00 00 00 00 08 88 99 AA BB CC DD EE FF'
+    # l = '00 00 00 00 00 08 97 9F FF 9B 97 0C A6 A4'
+    # l = len(l.split(' '))
+    # l = 14
     d = d.replace(' ', '')
     d = bytes.fromhex(d)
     c.cracker_serial_data(l, d)
