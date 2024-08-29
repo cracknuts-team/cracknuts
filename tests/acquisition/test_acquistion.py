55afad11a73ec1cd01faaaa34adfc297124dff87
Dingzb <dingzibiao@gmail.com>
2024-08-29 16:15:56 +0800

    add init do exception catch logic and run param.

--- tests/acquisition/test_acquistion.py
+++ tests/acquisition/test_acquistion.py
@@ -3,13 +3,12 @@ import time
 
 from cracknuts import logger
 from cracknuts.acquisition.acquisition import Acquisition
-from cracknuts.cracker.basic_cracker import BasicCracker
 from cracknuts.cracker.mock_cracker import MockCracker
 from cracknuts.cracker.stateful_cracker import StatefulCracker
 
 cracker = MockCracker()
 
-acq = Acquisition.builder().cracker(StatefulCracker(cracker)).init(lambda _: None).do(lambda _:None).build()
+acq = Acquisition.builder().cracker(StatefulCracker(cracker)).init(lambda _: None).do(lambda _: None).build()
 
 
 def setup_functions():
@@ -17,14 +16,21 @@ def setup_functions():
     logger.set_level(logging.DEBUG, cracker)
 
 
-def test_acquisition_test_mode():
-    # acq.on_wave_loaded(lambda waves: print(waves.shape))
-    acq.run(1000)
-    time.sleep(100000)
-    # print('acq', acq.get_last_wave().shape)
-    acq.stop()
+def test_acquisition_run_mode():
+    trace_count = 0
+
+    def count_plus(_):
+        nonlocal trace_count
+        trace_count += 1
+
+    acq.on_wave_loaded(count_plus)
+    acq.run(100)
+    while acq.get_status() != Acquisition.STATUS_STOPPED:
+        time.sleep(0.1)
+
+    assert trace_count == 100
 
 
 if __name__ == '__main__':
     setup_functions()
-    test_acquisition_test_mode()
+    test_acquisition_run_mode()
