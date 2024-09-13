1f0ab4d05e7b01b286498e99ff27b14420b36c4c
hexsine <hexsine@126.com>
2024-09-13 23:41:55 +0800

    test

--- src/cracknuts/cracker/basic_cracker.py
+++ src/cracknuts/cracker/basic_cracker.py
@@ -22,7 +22,7 @@ class CrackerS1(AbsCnpCracker):
         return res.decode("ascii") if res is not None else None
 
     def get_name(self) -> str:
-        res = self.send_and_receive(protocol.build_send_message(Commands.GET_ID))
+        res = self.send_and_receive(protocol.build_send_message(Commands.GET_NAME))
         return res.decode("ascii") if res is not None else None
 
     def scrat_analog_channel_enable(self, enable: dict[int, bool]):
