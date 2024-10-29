7dc19f24fa70e55baf3c93ebda26e830b6ff3c14
Dingzb <dingzibiao@gmail.com>
2024-10-29 17:36:17 +0800

    feat: add create ipynb from template.

--- src/cracknuts/__main__.py
+++ src/cracknuts/__main__.py
@@ -5,8 +5,16 @@ import logging
 import click
 
 import cracknuts
-from cracknuts.cracker import protocol
 import cracknuts.mock as mock
+from cracknuts.cracker import protocol
+
+try:
+    from cracknuts_panel import __main__ as cp_main
+
+    has_cracknuts_panel = True
+except ImportError:
+    cp_main = None
+    has_cracknuts_panel = False
 
 
 @click.group(help="A library for cracker device.", context_settings=dict(max_content_width=120))
@@ -25,11 +33,11 @@ def main(): ...
     type=int,
 )
 @click.option(
-    "--logging_level",
+    "--logging-level",
     default="INFO",
     show_default=True,
     help="The logging level of mock cracker.",
-    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=True),
 )
 def start_mock_cracker(
     host: str = "127.0.0.1",
@@ -38,6 +46,27 @@ def start_mock_cracker(
     logging_level: str | int = logging.INFO,
 ):
     mock.start(host, port, operator_port, logging_level)
+
+
+if has_cracknuts_panel:
+
+    @main.command(help="Create a jupyter notebook from template.")
+    @click.option(
+        "--template",
+        "-t",
+        help="The jupyter notebook template.",
+        required=True,
+        type=click.Choice(["acquisition", "analysis"]),
+    )
+    @click.option(
+        "--new-ipynb-name",
+        "-n",
+        "new_ipynb_name",
+        help="The jupyter notebook name or path.",
+        required=True,
+    )
+    def create_jupyter_notebook(template: str, new_ipynb_name: str):
+        cp_main.create_jupyter_notebook(template, new_ipynb_name)
 
 
 if __name__ == "__main__":
