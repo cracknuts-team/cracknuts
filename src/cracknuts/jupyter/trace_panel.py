bf177a5443f0937191c04c20c71099a781a3f96f
Dingzb <dingzibiao@gmail.com>
2024-12-11 16:42:41 +0800

    feat: monitor custom range.

--- src/cracknuts/jupyter/trace_panel.py
+++ src/cracknuts/jupyter/trace_panel.py
@@ -16,13 +16,11 @@ class TraceMonitorPanelWidget(MsgHandlerPanelWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "TraceMonitorPanelWidget.js"
     _css = ""
 
-    series_data = traitlets.Dict({1: [0 for _ in range(1000)]}).tag(sync=True)
+    series_data = traitlets.Dict({}).tag(sync=True)
 
-    custom_range_model = traitlets.Bool(False).tag(sync=True)
+    # custom_range_model = traitlets.Bool(False).tag(sync=True)
     custom_y_range: dict[str, tuple[int, int]] = traitlets.Dict({"1": (0, 0), "2": (0, 0)}).tag(sync=True)
-    y_range: dict[int, tuple[int, int, int]] = traitlets.Dict({1: (None, None, None), 2: (None, None, None)}).tag(
-        sync=True
-    )
+    y_range: dict[int, tuple[int, int]] = traitlets.Dict({1: (None, None), 2: (None, None)}).tag(sync=True)
     combine_y_range = traitlets.Bool(False).tag(sync=True)
 
     monitor_status = traitlets.Bool(False).tag(sync=True)
@@ -41,42 +39,51 @@ class TraceMonitorPanelWidget(MsgHandlerPanelWidget):
         self._trace_update_stop_flag = True
 
     def update(self, series_data: dict[int, np.ndarray]) -> None:
-        mn1, mx1, interval1 = None, None, None
-        mn2, mx2, interval2 = None, None, None
+        (
+            mn1,
+            mx1,
+        ) = None, None
+        (
+            mn2,
+            mx2,
+        ) = None, None
 
-        if self.custom_range_model:
-            mn1, mx1 = self.custom_y_range["1"]
-            mn2, mx2 = self.custom_y_range["2"]
-        else:
-            if 1 in series_data.keys():
-                c1 = series_data[1]
-                mn1, mx1 = np.min(c1), np.max(c1)
-            if 2 in series_data.keys():
-                c2 = series_data[2]
-                mn2, mx2 = np.min(c2), np.max(c2)
+        # if self.custom_range_model:
+        #     self.y_range = {
+        #         1: (self.custom_y_range["1"][0], self.custom_y_range["1"][1], None),
+        #         2: (self.custom_y_range["2"][0], self.custom_y_range["2"][1], None)
+        #     }
+        # else:
+        if 1 in series_data.keys():
+            c1 = series_data[1]
+            mn1, mx1 = np.min(c1), np.max(c1)
+        if 2 in series_data.keys():
+            c2 = series_data[2]
+            mn2, mx2 = np.min(c2), np.max(c2)
+            #
+            # if self.combine_y_range:
+            #     if mn1 is not None and mx1 is not None and mn2 is not None and mx2 is not None:
+            #         mn = min(mn1, mn2)
+            #         mx = max(mx1, mx2)
+            #     elif mn1 is None and mx1 is None and mn2 is not None and mx2 is not None:
+            #         mn, mx = mn2, mx2
+            #     elif mn1 is not None and mx1 is not None and mn2 is None and mx2 is None:
+            #         mn, mx = mn1, mx1
+            #     else:
+            #         mn, mx = None, None
+            #     if mn is not None and mx is not None:
+            #         mn, mx, interval = self._calculate_y_axis_ticks(mn, mx)
+            #         mn1, mn2 = mn, mn
+            #         mx1, mx2 = mx, mx
+            # else:
+            #     if 1 in series_data.keys():
+            #         mn1, mx1, interval1 = self._calculate_y_axis_ticks(mn1, mx1)
+            #
+            #     if 2 in series_data.keys():
+            #         mn2, mx2, interval2 = self._calculate_y_axis_ticks(mn2, mx2)
 
-        if self.combine_y_range:
-            if mn1 is not None and mx1 is not None and mn2 is not None and mx2 is not None:
-                mn = min(mn1, mn2)
-                mx = max(mx1, mx2)
-            elif mn1 is None and mx1 is None and mn2 is not None and mx2 is not None:
-                mn, mx = mn2, mx2
-            elif mn1 is not None and mx1 is not None and mn2 is None and mx2 is None:
-                mn, mx = mn1, mx1
-            else:
-                mn, mx = None, None
-            if mn is not None and mx is not None:
-                mn, mx, interval = self._calculate_y_axis_ticks(mn, mx)
-                mn1, mn2 = mn, mn
-                mx1, mx2 = mx, mx
-        else:
-            if 1 in series_data.keys():
-                mn1, mx1, interval1 = self._calculate_y_axis_ticks(mn1, mx1)
-
-            if 2 in series_data.keys():
-                mn2, mx2, interval2 = self._calculate_y_axis_ticks(mn2, mx2)
-
-        self.y_range = {1: (mn1, mx1, interval1), 2: (mn2, mx2, interval2)}
+            # self.y_range = {1: (mn1, mx1, interval1), 2: (mn2, mx2, interval2)}
+        self.y_range = {1: (mn1, mx1), 2: (mn2, mx2)}
 
         self.series_data = {k: v.tolist() for k, v in series_data.items()}
 
