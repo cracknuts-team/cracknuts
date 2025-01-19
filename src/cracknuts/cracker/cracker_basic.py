d2ac4fcdf18661211dde9d6b49483703f915d2d8
Dingzb <dingzibiao@gmail.com>
2025-01-19 16:38:59 +0800

    fix: Move logging level feature to cracker (#67)

--- src/cracknuts/cracker/cracker_basic.py
+++ src/cracknuts/cracker/cracker_basic.py
@@ -187,6 +187,23 @@ class CrackerBasic(ABC, typing.Generic[T]):
 
         self._server_address = host, int(port)
 
+    def set_logging_level(self, level: str) -> None:
+        """
+        Set logging level.
+
+        :param level: The logging level, debug, info, warning, error,
+        """
+
+        if level.lower() not in ("debug", "info", "warning", "error"):
+            self._logger.error("Invalid logging level.")
+            return False
+
+        status, res = self.send_and_receive(
+            protocol.build_send_message(protocol.Command.SET_LOGGING_LEVEL, payload=level.encode("ascii"))
+        )
+        if status != protocol.STATUS_OK:
+            self._logger.error(f"Failed to set logging level: {res}")
+
     def get_operator(self) -> Operator:
         """
         Get the operator object for this Cracker instance.
