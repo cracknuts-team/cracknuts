bb04e3550c2541107f10f8651cd68896fa52d693
Dingzb <dingzibiao@gmail.com>
2025-02-27 23:09:38 +0800

    feat: dev

--- tests/cracker/test_cracker_s1.py
+++ tests/cracker/test_cracker_s1.py
@@ -9,8 +9,6 @@ import cracknuts.mock as mock
 from cracknuts.cracker.cracker_s1 import CrackerS1
 from cracknuts.cracker.protocol import Command
 
-device = CrackerS1(address=('localhost', 9761))
-
 
 @pytest.fixture(scope='module')
 def mock_cracker():
@@ -23,29 +21,32 @@ def mock_cracker():
     yield
 
 
-def get_result_by_command(command):
+@pytest.fixture(scope='module')
+def cracker_s1(mock_cracker):
+    device = CrackerS1(address=('localhost', 9761))
+    # set_level(logging.INFO, device)
+    device.connect(update_bin=False)
+    yield device
+    device.disconnect()
+
+
+def get_result_by_command(device, command):
     _, r = device.send_with_command(0xFFFF, payload=struct.pack('>I', command))
     return r
 
+def test_nut_voltage(cracker_s1):
+    s, _ = cracker_s1.nut_voltage("3.3")
+    assert s == 0 and cracker_s1.get_current_config().nut_voltage == 3.3
+    assert get_result_by_command(cracker_s1, Command.NUT_VOLTAGE) == struct.pack('>I', 3300)
 
-def setup_function(mock_cracker):
-    # set_level(logging.INFO, device)
-    device.connect(update_bin=False)
+    s, _ = cracker_s1.nut_voltage("3.4v")
+    assert s == 0 and cracker_s1.get_current_config().nut_voltage == 3.4
+    assert get_result_by_command(cracker_s1, Command.NUT_VOLTAGE) == struct.pack('>I', 3400)
 
+    s, _ = cracker_s1.nut_voltage("3500mV")
+    assert s == 0 and cracker_s1.get_current_config().nut_voltage == 3.5
+    assert get_result_by_command(cracker_s1, Command.NUT_VOLTAGE) == struct.pack('>I', 3500)
 
-def test_nut_voltage(mock_cracker):
-    s, _ = device.nut_voltage("3.3")
-    assert s == 0 and device.get_current_config().nut_voltage == 3.3
-    assert get_result_by_command(Command.NUT_VOLTAGE) == struct.pack('>I', 3300)
-
-    s, _ = device.nut_voltage("3.4v")
-    assert s == 0 and device.get_current_config().nut_voltage == 3.4
-    assert get_result_by_command(Command.NUT_VOLTAGE) == struct.pack('>I', 3400)
-
-    s, _ = device.nut_voltage("3500mV")
-    assert s == 0 and device.get_current_config().nut_voltage == 3.5
-    assert get_result_by_command(Command.NUT_VOLTAGE) == struct.pack('>I', 3500)
-
-    s, _ = device.nut_voltage("3.6")
-    assert s == 0 and device.get_current_config().nut_voltage == 3.6
-    assert get_result_by_command(Command.NUT_VOLTAGE) == struct.pack('>I', 3600)
+    s, _ = cracker_s1.nut_voltage("3.6")
+    assert s == 0 and cracker_s1.get_current_config().nut_voltage == 3.6
+    assert get_result_by_command(cracker_s1, Command.NUT_VOLTAGE) == struct.pack('>I', 3600)
