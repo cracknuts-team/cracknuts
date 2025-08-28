d76d00daf8469485751df7e20fdddde7ec504d76
Dingzb <dingzibiao@gmail.com>
2025-08-28 17:42:07 +0800

    dev: Add logic to update existing plots when show_trace is called.

--- src/cracknuts/trace/plot.py
+++ src/cracknuts/trace/plot.py
@@ -3,7 +3,7 @@
 import numpy as np
 import plotly.graph_objects as go
 from numpy.typing import ArrayLike
-from plotly_resampler import FigureResampler
+from plotly_resampler import FigureWidgetResampler
 
 from cracknuts.trace.trace import TraceDataset
 
@@ -17,16 +17,16 @@ class TracePlot:
         self._trace_dataset: TraceDataset | None = None
         self._traces: ArrayLike | None = None
         self._trace_name_prefix: list[str] | None = None
-        self._fig = FigureResampler(go.Figure())
-        self._fig.update_xaxes(
-            showline=True,  # 显示 x 轴外框
-            linewidth=1,  # 外框宽度
-            linecolor="#dddddd",  # 外框颜色
-            mirror=True,  # 上下两边都画
-            gridwidth=1,  # 网格线宽度
+        self._fig = FigureWidgetResampler(go.Figure())
+        self._fig.update_xaxes(  # type: ignore
+            showline=True,
+            linewidth=1,
+            linecolor="#dddddd",
+            mirror=True,
+            gridwidth=1,
             gridcolor="#dddddd",
         )
-        self._fig.update_yaxes(
+        self._fig.update_yaxes(  # type: ignore
             showline=True, linewidth=1, linecolor="#dddddd", mirror=True, gridwidth=1, gridcolor="#dddddd"
         )
 
@@ -34,9 +34,7 @@ class TracePlot:
             plot_bgcolor="white",  # 绘图区背景白色
             paper_bgcolor="white",  # 整个画布背景白色
         )
-        self._fig.update_layout(
-            margin=dict(t=40, b=40, l=0)  # t=top, b=bottom
-        )
+        self._fig.update_layout(margin=dict(t=40, b=40, l=0))
         self._fig.update_layout(height=350)
         self._has_plot = False
 
@@ -48,22 +46,29 @@ class TracePlot:
         """
         self._trace_dataset = trace_dataset
 
-    def plot_line(self) -> FigureResampler:
+    def plot_line(self) -> FigureWidgetResampler:
         """
         显示曲线， 默认不通过配置 show_trace 配置显示曲线时，则显示 通道 0 的所有曲线
         """
         if self._traces is None:
             self.show_trace()
+        self._fig.data = []
         for i in range(self._traces.shape[0]):
             if self._trace_name_prefix is not None and i < len(self._trace_name_prefix):
                 trace_name = self._trace_name_prefix[i] + "/"
             else:
                 trace_name = ""
             trace_name += f"t-{i}"
-            self._fig.add_trace(
-                go.Scattergl(name=trace_name, showlegend=True, line={"width": 1}, mode="lines"),
-                hf_y=self._traces[i, :],
-            )
+            # self._fig.add_trace(
+            #     go.Scattergl(name=trace_name, showlegend=True, line={"width": 1}, mode="lines"),
+            #     hf_y=self._traces[i, :],
+            # )
+        traces_list = [
+            go.Scattergl(name=f"trace {i}", showlegend=True, line={"width": 1}, mode="lines", y=self._traces[i, :])
+            for i in range(self._traces.shape[0])
+        ]
+        self._fig.add_traces(traces_list)
+        self._fig.reload_data()
         self._has_plot = True
         return self._fig
 
@@ -76,6 +81,7 @@ class TracePlot:
         :param t_idx: 曲线切片，当传入切片列表时，列表与 ch_idx 列表匹配进行切片
         :type t_idx: list[slice, slice]
         """
+
         if isinstance(ch_idx, int):
             traces = self._trace_dataset.get_origin_data()[f"0/{ch_idx}/traces"]
             if t_idx is None:
@@ -98,3 +104,6 @@ class TracePlot:
                 traces.append(trace)
                 name_ch_prefixes.append([f"c-{_ch_idx}" for _ in range(trace.shape[0])])
             self._traces = np.vstack(traces)
+
+        if self._has_plot:
+            self.plot_line()
